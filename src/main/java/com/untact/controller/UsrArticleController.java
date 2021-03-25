a9a9939c1b4ef75771d58481434e24e8bf17b580
AnChangHyune <ymy6534@gmail.com>
2021-03-25 17:24:42 +0900

    게시물 추가, 게시물 삭제

--- src/main/java/com/untact/controller/UsrArticleController.java
+++ src/main/java/com/untact/controller/UsrArticleController.java
@@ -1,7 +1,9 @@
 package com.untact.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,11 @@ import com.untact.dto.Article;
 
 @Controller
 public class UsrArticleController {
+	private int articlesLastId;
 	private List<Article> articles;
 	
 	public UsrArticleController() {
+		articlesLastId = 0;
 		articles =  new ArrayList<>();
 		
 		articles.add(new Article(1, "2021.03.25 16:39:12","제목1","내용1"));
@@ -30,5 +34,46 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> showList() {
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Map<String, Object> doAdd(String regDate, String title, String body) {
+		articles.add(new Article(++articlesLastId, regDate, title, body));
+		
+		Map<String, Object> rs = new HashMap<>();
+		rs.put("resultCode", "S-1");
+		rs.put("msg", "성공하였습니다.");
+		rs.put("id", articlesLastId);
+		
+		return rs;
+	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public Map<String, Object> doDelete(int id) {
+		boolean deleteArticleRs = deleteArticle(id);
+		
+		Map<String, Object> rs = new HashMap<>();
+		if(deleteArticleRs) {
+			rs.put("resultCode", "S-1");
+			rs.put("msg", "성공하였습니다.");
+		}else {
+			rs.put("resultCode", "F-1");
+			rs.put("msg", "해당 게시물은 존재하지 않습니다.");
+		}
+		
+		rs.put("id", id);
+		
+		return rs;
+	}
+	private boolean deleteArticle(int id) {
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				articles.remove(article);
+				return true;
+			}
+		}
+		return false;
 	}
 }
